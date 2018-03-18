5bf525be4f5c1ae4c1df032dc609ba4d99512535
leian7 <anne.lei@scaleft.com>
2018-03-17 20:33:15 -0700

    add standard network utils

---
+++ app/src/main/java/com/cogwerks/potato/utils/NetworkUtils.java
@@ -0,0 +1,28 @@
+package com.cogwerks.potato.utils;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * Created by annie on 3/17/18.
+ */
+
+public class NetworkUtils {
+    private static final OkHttpClient mHTTPClient = new OkHttpClient();
+
+    public static String doHTTPGet(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = mHTTPClient.newCall(request).execute();
+
+        try {
+            return response.body().string();
+        } finally {
+            response.close();
+        }
+    }
+}
