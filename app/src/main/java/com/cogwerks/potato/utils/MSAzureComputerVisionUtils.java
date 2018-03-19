394a23d98497930a2d77702507daf3d615fa9bcb
leian7 <anne.lei@scaleft.com>
2018-03-19 01:40:13 -0700

    parse JSON for tags and confidences

--- app/src/main/java/com/cogwerks/potato/utils/MSAzureComputerVisionUtils.java
+++ app/src/main/java/com/cogwerks/potato/utils/MSAzureComputerVisionUtils.java
@@ -2,6 +2,13 @@ package com.cogwerks.potato.utils;
 
 import android.net.Uri;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
 /**
  * Created by annie on 3/17/18.
  */
@@ -10,13 +17,18 @@ public class MSAzureComputerVisionUtils {
     private final static String CV_ANALYZE_BASE_URL = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
 
     private final static String CV_ANALYZE_FEATURES_PARAM = "visualFeatures";
-    private final static String CV_ANALYZE_FEATURES_VALUE = "Tags";
+    private final static String CV_ANALYZE_FEATURES_VALUE = "Tags,Description,Color,Faces,Adult";
 
     private final static String CV_ANALYZE_DETAILS_PARAM = "details";
-    private final static String CV_ANALYZE_DETAILS_VALUE = "Celebrities";
+    private final static String CV_ANALYZE_DETAILS_VALUE = "Celebrities,Landmarks";
 
     private final static String CV_ANALYZE_API_KEY_PARAM = "subscription-key";
     private final static String CV_ANALYZE_API_KEY_VALUE = "a97aab14e3264b74a601e0c38cfcd9bc";
+
+    public static class AnalyzeResult implements Serializable {
+        public String tag;
+        public String confidence;
+    }
 
     public static String buildAnalyzeURL() {
         return Uri.parse(CV_ANALYZE_BASE_URL).buildUpon()
@@ -25,5 +37,24 @@ public class MSAzureComputerVisionUtils {
                 .appendQueryParameter(CV_ANALYZE_API_KEY_PARAM, CV_ANALYZE_API_KEY_VALUE)
                 .build()
                 .toString();
+    }
+
+    public static ArrayList<AnalyzeResult> parseAnalyzeResultsJSON(String resultsJSON) {
+        try {
+            JSONObject resultsObj = new JSONObject(resultsJSON); // grab entire string as JSON object
+            JSONArray resultsItems  = resultsObj.getJSONArray("tags"); // grab tags array
+
+            ArrayList<AnalyzeResult> analyzeResultsList = new ArrayList<AnalyzeResult>();
+            for (int i = 0; i < resultsItems.length(); i++) { // for each key/val object in tags,
+                AnalyzeResult result = new AnalyzeResult();
+                JSONObject resultItem = resultsItems.getJSONObject(i); // grab {tag_name: confidence} object
+                result.tag = resultItem.getString("name");
+                result.confidence = resultItem.getString("confidence");
+                analyzeResultsList.add(result);
+            }
+            return analyzeResultsList;
+        } catch (JSONException e) {
+            return  null;
+        }
     }
 }
