3e6777bdc0054f4197f9c71969a99af4e2125cb7
Duncan Millard <duncan.millard@tektronix.com>
2018-03-19 12:47:15 -0700

    Adding camera intent with minor refactor of gallery intent

--- app/src/main/java/com/cogwerks/potato/MainActivity.java
+++ app/src/main/java/com/cogwerks/potato/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.FileProvider;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,8 +21,10 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
@@ -29,12 +33,17 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private ImageButton mChooseGallery;
     private ImageButton mChooseCamera;
     private int PICK_IMAGE_REQUEST = 1;
+    private int CAMERA_RES_REQUEST = 2;
+
     private static final String IMAGE_TYPE = "image/*";
     private static final String TAG = MainActivity.class.getSimpleName();
     private TextView mLoadingErrorMessageTV;
 
+    private Uri mPhotoUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "Startup");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -67,7 +76,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         });
 
         mChooseCamera = (ImageButton) findViewById(R.id.ib_camera_btn);
-        //Add onClickListener for camera button here
+        mChooseCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCameraIntent();
+            }
+        });
 
     }
 
@@ -92,14 +106,28 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data.getData() != null){
             Uri uri = data.getData();
             try{
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                ImageView imageView = (ImageView) findViewById(R.id.iv_user_image);
-                imageView.setImageBitmap(bitmap);
+                updateThumbnailPicture(uri);
                 //send this data to our api
             } catch (IOException e) {
-                Log.d("Error: ", "Failed to receive image. Please try again");
+                Log.d(TAG, "Failed to receive gallery image. Please try again");
             }
         }
+        else if(requestCode == CAMERA_RES_REQUEST){
+            try{
+                updateThumbnailPicture(mPhotoUri);
+            } catch (IOException e) {
+                Log.d(TAG, "Failed to receive camera image. Please try again");
+            }
+
+
+        }
+    }
+
+    private void updateThumbnailPicture(Uri imageUri) throws IOException
+    {
+        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+        ImageView imageView = (ImageView) findViewById(R.id.iv_user_image);
+        imageView.setImageBitmap(bitmap);
     }
 
     @Override
@@ -128,4 +156,52 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void onLoaderReset(Loader loader) {
 
     }
+
+
+
+    private void launchCameraIntent() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // If there is no app to handle the camera, bail out
+        if (cameraIntent.resolveActivity(getPackageManager()) == null)
+        {
+            Log.d(TAG, "No camera app located. Cancelling request.");
+            return;
+        }
+
+        //Allocate URI for camera intent to use
+        File cameraFile = null;
+        try {
+            cameraFile = createImageFile();
+        } catch (IOException e) {
+            Log.d(TAG, "Error while creating image file: " + e.getMessage());
+        }
+
+        if (cameraFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.cogwerks.fileprovider", cameraFile);
+            mPhotoUri = photoURI;
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(cameraIntent, CAMERA_RES_REQUEST);
+        }
+
+    }
+
+    //Based on sample code from https://developer.android.com/training/camera/photobasics.html
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",   /* suffix */
+                storageDir      /* directory */
+        );
+        Log.d(TAG, "Created file: " + image);
+        // Save a file: path for use with ACTION_VIEW intents
+        return image;
+    }
+
+
 }
